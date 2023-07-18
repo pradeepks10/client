87fd6fa2f2a9c3642964f53861bbd8c99a230b9d
Pradeep Kumar Singh <88943381+pradeepks10@users.noreply.github.com>
2023-07-18 20:05:06 +0530

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -48,7 +48,7 @@ const logout = () => {
     return (
         <AppBar className={classes.appBar} position='static' color='inherit'>
                 <div className={classes.brandContainer}>
-                    <Typography component={Link} to="/" className={classes.heading} variant='h2' align='center'>Memories</Typography>
+                    <Typography component={Link} to="/" className={classes.heading} variant='h2' align='center'>Socio-BLOG</Typography>
                     <img  className={classes.image} src ={memories} alt='memories' height='60' />
                 </div>  
                 <Toolbar>
